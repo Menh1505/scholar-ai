8d8141a8ad44ec1ffccdbfcd825a4529df58b7d4
menh <dinhthienmenh1505@gmail.com>
2025-07-16 10:35:18 +0700

    fix legal page

---
+++ ui/src/components/legal/DocumentRequirement.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Fieldset from "../Fieldset";
+
+export const DocumentRequirement = () => {
+  return (
+    <Fieldset title="Document Requirement" className="h-full w-full">
+      Document
+    </Fieldset>
+  );
+};
---
+++ ui/src/components/legal/TaxInsurance.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Fieldset from "../Fieldset";
+
+export const TaxInsurance = () => {
+  return (
+    <Fieldset title="Tax & Insurance" className="h-full w-full">
+    </Fieldset>
+  );
+};
